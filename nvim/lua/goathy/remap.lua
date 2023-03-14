bcc60fbffc2ec7625b8d5647af1e7ae0905ef1f5
Jakub Gałecki <goathy.dev@gmail.com>
2023-03-14 19:06:29 +0000

    feat(neovim): add neovim config

---
+++ nvim/lua/goathy/remap.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader = " "
+
+-- Explorer keymap
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Reload config
+vim.keymap.set("n", "<leader><leader>", function ()
+	vim.cmd("so")
+end)
