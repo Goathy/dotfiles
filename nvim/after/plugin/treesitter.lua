52744b3d5d0506ac44b2d03ad6635c285d742e89
Jakub Gałecki <goathy.dev@gmail.com>
2023-03-15 12:47:01 +0100

    add missing comma

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = { "help", "vim", "bash", "lua", "gitignore", "yaml", "toml" }
+	ensure_installed = { "help", "vim", "bash", "lua", "gitignore", "yaml", "toml" },
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
 
