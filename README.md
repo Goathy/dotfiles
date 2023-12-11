87e452a50218583dfb93997332784ee4154b1c54
Jakub Gałecki <goathy.dev@gmail.com>
2023-12-11 11:50:25 +0100

    Add neovim config. Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Install zshenv
-ln <directory>/zshenv $HOME/.zshenv
+ln <destination>/zshenv <target>/.zshenv
+
+# Install Neovim config
+ln -s <destination>/config/nvim <target>/.config/nvim
 
---
+++ config/nvim/init.lua
@@ -0,0 +1,11 @@
+function noop () end
+
+local modes = { "i", "n", "v" }
+local keys = { "<Up>", "<Down>", "<Left>", "<Right>", "<BS>" }
+for _, key in ipairs(keys) do
+	vim.keymap.set(modes, key, noop)
+end
+
+vim.o.relativenumber = true
+vim.o.number = true
+vim.o.colorcolumn = "80"
