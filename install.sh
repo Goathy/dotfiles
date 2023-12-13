0c1d3b1f09e2b66b78f2387aed2a74cd7e246014
Jakub Gałecki <goathy.dev@gmail.com>
2023-12-13 13:08:58 +0100

    wrap install script in function

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-PWD=$(realpath $0)
-CWD=$(dirname $PWD)
+install() {
+	local PWD=$(realpath $0)
+	local CWD=$(dirname $PWD)
 
-source "$CWD/zshenv"
+	source "$CWD/zshenv"
 
-source "$CWD/install/install_xdg.sh"; install_xdg
-source "$CWD/install/install_n.sh"; install_n
-source "$CWD/install/install_zsh.sh"; install_zsh $CWD
+	source "$CWD/install/install_xdg.sh"; install_xdg
+	source "$CWD/install/install_n.sh"; install_n
+	source "$CWD/install/install_zsh.sh"; install_zsh $CWD
+}
 
+install
