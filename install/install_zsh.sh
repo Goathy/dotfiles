f60b1bc4d7a286f7204dfddde4bfce7b6c60442b
Jakub Gałecki <goathy.dev@gmail.com>
2023-12-13 12:56:01 +0100

    Add user

--- install/install_zsh.sh
+++ install/install_zsh.sh
@@ -2,6 +2,8 @@
 
 install_zsh() {
 	local PWD="$1"
+	local ZSH="$(which zsh)"
+	local USER="$(id -u -n)"
 
 	if ! command -v zsh&> /dev/null; then
 		echo "Error: zsh is not installed."
@@ -11,6 +13,6 @@ install_zsh() {
 	ln -s "$PWD/zshenv" "$HOME/.zshenv"
 	ln -s "$PWD/zsh" "$ZDOTDIR"
 
-	chsh -s "$(which zsh)"
+	chsh -s "$ZSH" "$USER"
 }
 
