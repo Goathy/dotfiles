389c7b278c4dbefd9a332b4857c0664daf75af94
Jakub Gałecki <goathy.dev@gmail.com>
2023-12-13 21:56:14 +0100

    exit if user is not sudo

--- install.sh
+++ install.sh
@@ -12,4 +12,11 @@ install() {
 	source "$CWD/install/install_zsh.sh"; install_zsh $CWD
 }
 
+is_sudo() {
+	if ["$(id -u)" -ne 0 ]; then
+		echo "The script is running as root, so you have sudo privileges."
+		exit 1
+	fi 
+}
+
 install
