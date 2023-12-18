5045e21a96316fc8410da4f65ac59f3e62e684bc
Jakub Gałecki <goathy.dev@gmail.com>
2023-12-18 14:20:16 +0100

    Load windows specific aliases only on wsl

--- zsh/aliases/wsl.sh
+++ zsh/aliases/wsl.sh
@@ -1,3 +1,5 @@
-alias wcopy="$(which clip.exe)"
-alias wpaste="$(which powershell.exe) Get-Clipboard | sed \"s/\r$//\""
+if uname -r | grep -q "microsoft"; then
+    alias wcopy="$(which clip.exe)"
+    alias wpaste="$(which powershell.exe) Get-Clipboard | sed \"s/\r$//\""
+fi
 
