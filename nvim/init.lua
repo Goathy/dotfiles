48a9d169ee5cfa12ed2b5bf137d70daa93c111fe
Jakub Gałecki <goathy.dev@gmail.com>
2023-12-18 14:25:10 +0100

    Update neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,37 @@
-function noop () end
+-- MODES --
+local NORMAL = "n"
+local INSERT = "i"
+local VISUAL = "v"
 
-local modes = { "i", "n", "v" }
-local keys = { "<Up>", "<Down>", "<Left>", "<Right>", "<BS>" }
-for _, key in ipairs(keys) do
-	vim.keymap.set(modes, key, noop)
-end
-
+-- OPTIONS --
+vim.o.scrolloff = 8
 vim.o.relativenumber = true
 vim.o.number = true
 vim.o.colorcolumn = "80"
-vim.g.mapleader=" "
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.smartindent = true
+
+-- COLOR SCHEME --
+vim.cmd("colorscheme desert")
+
+-- CUSTOM SHORTCUTS --
+local LEADER = " "
+
+vim.g.mapleader = LEADER
+vim.api.nvim_set_keymap(NORMAL, '<leader>pv', ':Vex<CR>', { noremap = true })
+
+-- DISABLE KEYS --
+function noop () end
+
+function disable_keys ()
+    local modes = { INSERT, INSERT, VISUAL }
+    local keys = { "<Up>", "<Down>", "<Left>", "<Right>", "<BS>" }
+    for _, key in ipairs(keys) do
+        vim.keymap.set(modes, key, noop)
+    end
+end
+
+disable_keys()
 
