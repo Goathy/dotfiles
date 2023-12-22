24db034c3518b7580f095512ed28bd9876c198f3
Jakub Gałecki <goathy.dev@gmail.com>
2023-12-22 15:32:06 +0100

    Add remap for Ex command

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,13 +21,15 @@ vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.smartindent = true
+vim.opt.termguicolors = true
 
 -- COLOR SCHEME --
-vim.cmd("colorscheme desert")
+-- vim.cmd("colorscheme desert")
 
 -- CUSTOM SHORTCUTS --
 local LEADER = " "
 
 vim.g.mapleader = LEADER
-vim.api.nvim_set_keymap(NORMAL, '<leader>pv', ':Vex<CR>', { noremap = true })
+vim.api.nvim_set_keymap(NORMAL, '<leader>ex', ':Ex<CR>', { noremap = true })
+vim.api.nvim_set_keymap(NORMAL, '<leader><CR>', ':so $XDG_CONFIG_HOME/nvim/init.lua<CR>', { noremap = true, silent = true })
 
